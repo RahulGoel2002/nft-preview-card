c044f6c620caaa6fcc02ed7da8c131262ec4df90
RahulGoel2002 <rgoel1608@gmail.com>
2023-07-08 02:10:38 +0530

    portfolio

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -3,9 +3,16 @@ import "./App.css"
 import Card from '../Card/Card';
 
 const App = () => {
+    const s = [null, null, null, null, null, null]
     return (
         <div className='app'>
-            <Card />
+            {
+                s.map(
+                    p => {
+                        return <div className="card-wrapper"><Card /></div>
+                    }
+                )
+            }
         </div>
     );
 }
